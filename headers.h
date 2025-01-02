d3bb802e3cf9794c2c5fb03dd0464f6c1fa6a206
Gift <102348185+draw123draw@users.noreply.github.com>
2025-01-02 18:49:32 +0800

    Update headers.h

--- headers.h
+++ headers.h
@@ -9,7 +9,7 @@
 #include<FL/Fl_Button.H>
 #include<FL/Fl_Input.H>
 #include<FL/Fl_Menu_Bar.H>
-#include<FL/Fl_Value_Slider.H>
+#include<FL/Fl_Hor_Slider.H>
 #include<FL/Fl_Counter.H>
 #include<FL/Fl_Int_Input.H>
 #include<FL/Fl_Text_Editor.H>
@@ -19,6 +19,7 @@
 #include<FL/Fl_Chart.H>
 #include<FL/Fl_Table_Row.H>
 #include<FL/Fl_Tabs.H>
+#include<FL/Fl_Progress.H>
 #include<FL/Fl_Tooltip.H>
 
 #include<FL/fl_ask.H>
@@ -46,7 +47,11 @@ class ColorPoint;
 class Imagesc;
 class FreeCounter;
 class FreeCounterHeader;
+class Property;
+class Converter;
+class Cutter;
 class App;
+class BinPara;
 class HeaderTable;
 class VHeaderTable;
 
@@ -75,16 +80,13 @@ public:
     int handle(int event)override;
 };
 
-// class
-
 class Imagesc:public Fl_Gl_Window
 {
 public:
     float *mat;
     float max_a,min_a,max_abs_a=0;
     float max_s,min_s,max_abs_s=0;
-    bool set_mat=false;
-    bool centered_norm=false;
+    bool set_mat=false,c_norm=true;
     int rows,cols;
     void draw()override;
     Imagesc(int x,int y,int width,int height);
@@ -95,21 +97,40 @@ public:
 class FreeCounter:public Fl_Counter
 {
     static void tc_cb(Fl_Widget *w,void *data);
+    static void tc_cb_bin(Fl_Widget *w,void *data);
     static void ok_cb(Fl_Widget *w,void *data);
 public:
     int step_s,step_l;
     Fl_Window *counter_set;
-    Fl_Int_Input *fi_curr;
-    Fl_Int_Input *fi_step_s;
-    Fl_Int_Input *fi_step_l;
+    Fl_Int_Input *fi_curr,*fi_step_s,*fi_step_l;
     Fl_Button *set_ok;
     FreeCounter(int x,int y,int w,int h);
     int handle(int event)override;
 };
 
+class Property:public Fl_Window
+{
+public:
+    float fmax=FLT_MIN,fmin=FLT_MAX;
+    static void global_but_cb(Fl_Widget *,void *);
+    Fl_Box *text_box;
+    Fl_Box *global_box;
+    Fl_Button *global_but;
+    bool stop;
+    char property_text[512],global_text[512];
+    Property(int W,int H,const char *title);
+};
+
+class Progress:public Fl_Progress
+{
+public:
+    Progress(int X,int Y,int W,int H);
+    bool stop=false;
+    int handle(int event)override;
+};
+
 class FreeCounterHeader:public Fl_Counter
 {
-    static void tc_cb(Fl_Widget *w,void *data);
 public:
     Fl_Window *counter_set;
     Fl_Int_Input *fi_curr;
@@ -118,35 +139,54 @@ public:
     int handle(int event)override;
 };
 
+class Cutter:public Fl_Window
+{
+    static void cut_cb(Fl_Widget *,void *);
+    static void split_cb(Fl_Widget *,void *);
+public:
+    unsigned char hdr[3600];
+    char outfilename[256],outfolder[256];
+    Fl_Button *cut_but,*split_but;
+    Fl_Button *open_file_cut,*open_file_split;
+    Fl_Tabs *tabs;
+    Fl_Group *CutGroup,*Split_Group;
+    Fl_Input *input_cut,*input_split;
+    Fl_Int_Input *n_parts_input,*n_traces_input;
+    Fl_Int_Input *start_trace_input,*end_trace_input,*interval_trace_input;
+    Fl_Int_Input *start_sample_input,*end_sample_input,*interval_sample_input;
+    Cutter(int W,int H,const char *title);
+};
+
+class Converter:public Fl_Window
+{
+    static void format_cb(Fl_Widget *,void *);
+    static void convert_cb(Fl_Widget *,void *);
+public:
+    bool sun2pc;
+    char outfilename[256];
+    Fl_Button *ok;
+    Fl_Box *notes;
+    Fl_Input *input;
+    Fl_Button *open_file;
+    Converter(int W,int H,const char *title);
+};
+
 class App:public Fl_Window
 {
-    int open_x=120;//打开文件按钮
-    int open_y=50;
-
-    unsigned char hdr[3600];
+    int open_x=120,open_y=50;//打开文件按钮
     char mxy[64],amp_c[64];
     bool first_read=true;
-    bool is_le;
-    bool is_pc;
-    Fl_Menu_Bar *menus;
     Fl_Text_Editor *hdr_text;
     Fl_Text_Buffer *tbuff;
-    Fl_Button* open_button;
-    Fl_Button *save_hdr;
+    Fl_Button *open_button;
 
-    Fl_Window* CroppingWin=NULL;
-    Fl_Window* SaveWin=NULL;
-    Fl_Window* TextWin=NULL;
-    Fl_Window* HdrWin=NULL;
-    Fl_Window *PropertyWin=NULL;
+    Fl_Window *CroppingWin=NULL,*TextWin=NULL,*HdrWin=NULL,*ColorbarWin=NULL;
     Fl_Native_File_Chooser *fc,*fcc,*fcd;
     Fl_Input_Choice *colormap_c;
     static void start_cb(Fl_Widget* widget,void *);
     static void ts_cb(Fl_Widget *widget,void *);
-    static void save_cb(Fl_Widget *widget,void *);
-    static void property_cb(Fl_Widget *widget,void *);
+    static void ts_cb_bin(Fl_Widget *widget,void *);
     static void hdr_cb(Fl_Widget *widget,void *);
-    static void cutter_separater_cb(Fl_Widget *widget,void *);
     static void format_cb(Fl_Widget *widget,void *);
     static void endian_cb(Fl_Widget *widget,void *);
     static void enhance_cb(Fl_Widget *widget,void *);
@@ -155,10 +195,15 @@ class App:public Fl_Window
     static void save_clr_cb(Fl_Widget *widget,void *);
     static void save_sgy_data_cb(Fl_Widget *w,void *);
 public:
-    char filename[100];
+    Fl_Menu_Bar *menus;
+    bool is_bin=false;
+    char filename[256],traces_c[32],samples_c[32],ld4_c[64];
+    char fmt,fmk;
+    unsigned char hdr[3600];
     struct manynames filenames;
     int *histo;
     FILE *fpi,*fpo,*fpd;
+    unsigned long long l;
     long long traces;
     int trace_s,samples,enhance_cnt=0;
     float *seis=NULL;
@@ -166,28 +211,35 @@ public:
 
     int height_of_menu=30;
     Fl_Chart *chart=NULL;
-    const int bin_num=60;
-    Fl_Window *ColorbarWin=NULL;
+    const int nbin=60;
     Fl_Box *where_mouse_box=NULL;
-    Fl_Value_Slider *trace_slider;
+    Fl_Input *input_load_hdr;
+    Fl_Hor_Slider *trace_slider;
     FreeCounter *trace_counter;
-    Fl_Int_Input *n_parts_input;
-    Fl_Int_Input *n_traces_input;
-    Fl_Int_Input *start_trace_input;
-    Fl_Int_Input *end_trace_input;
-    Fl_Int_Input *interval_trace_input;
-    Fl_Int_Input *start_sample_input;
-    Fl_Int_Input *end_sample_input;
-    Fl_Int_Input *interval_sample_input;
 
     App(int X,int Y,int Width,int Height,const char *title);
     ~App();
+    void close_them(bool all=false);
     int handle(int event)override;
-    void swap_bytes();
+    void swap_bytes(float *seis_b,int length);
+    void detect_nan();
     void open_figure();
     void change_profile();
-    void read_data(const char *fname);
-    void ieee2ibm();//公式已经考虑了字节序，无需再用swap_bytes转换
+    void change_profile_bin();
+    void format_correct(float *seis_,int length);
+    void read_data(const char *fname,bool utf_flag=true);
+    void ieee2ibm(float *seis_b,int length);//公式已经考虑了字节序，无需再用swap_bytes转换
+};
+
+class BinPara:public Fl_Window
+{
+    Fl_Box *notes;
+    Fl_Button *guess,*ok;
+    char notes_c[512];
+public:
+    bool pass=false;
+    Fl_Int_Input *traces_input,*samples_input;
+    BinPara(int W,int H,const char* title);
 };
 
 class HeaderTable:public Fl_Table_Row
@@ -195,9 +247,7 @@ class HeaderTable:public Fl_Table_Row
     void draw_cell(TableContext context,int ROW=0,int COL=0,int X=0,int Y=0,int W=0,int H=0)override;
     void DrawHeader(const char *s,int X,int Y,int W,int H);
 public:
-    int trace_s_h=5;
-    int selected_R=1;
-    int bytes_num=4;
+    int trace_s_h=5,selected_R=1,bytes_num=4;
     long long trace_num=0;
     unsigned char *hdrbytes_s;
     void get_hdr();
@@ -224,13 +274,18 @@ public:
 };
 
 App *app;
-ColorPoint **colors=nullptr;
-ColorPoint *tmp_c=nullptr;
-ColorBar *colorbar=nullptr;
-Imagesc *fwin=nullptr;
-HeaderTable *headertable=nullptr;
-VHeaderTable *vht=nullptr;
-ResultTable *rt=nullptr;
+BinPara *binpara;
+ColorPoint **colors=NULL;
+ColorPoint *tmp_c=NULL;
+ColorBar *colorbar=NULL;
+Imagesc *fwin=NULL;
+Property *property=NULL;
+Progress *progress=NULL;
+Cutter *cutter=NULL;
+Converter *converter=NULL;
+HeaderTable *headertable=NULL;
+VHeaderTable *vht=NULL;
+ResultTable *rt=NULL;
 void findnames(char* filename,manynames* outfile);
 
 #endif
